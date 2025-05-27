caa0737e6daed8ee335ff9ad7ebd76bdc085b4bf
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-27 13:54:24 +0100

    add a simple steps to run the application.

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -15,6 +15,10 @@ public class HomeController {
      * so it will be decorated with the 
      * to view this in action, go to the resource folder, we created a static/root-main.html which 
      * servers as our views.
+     * to run this project, run mvn:spring-boot:runon your terminal.
+     * carefully observe and see this, 'Tomcat initialized with port 8080 (http)' which signified your project is running on localhost
+     * port 8080
+     * go to your browser and type 'http://localhost:8080', and your are up and running. you just build your first spring-boot application
      */
     @RequestMapping("/")
     public String main() {
