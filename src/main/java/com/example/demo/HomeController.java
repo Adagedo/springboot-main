551f4ed4711f46d3519f27fe88d58d8ff92d1178
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-27 13:37:16 +0100

    implemented a simple controller known as homecontroller

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * to make the HomeController a valid controller, we decorate it with the 
+ * @Controller annotation.
+ * 
+ */
+@Controller
+public class HomeController {
+    /*
+     * when we send a request to the root of our website, we want this method to be called.
+     * so it will be decorated with the 
+     */
+    @RequestMapping("/")
+    public String main() {
+        return "root-main.html";
+    }
+}
