39cd8651a1f06cbcc9136e3349a99646c9865fe3
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-31 15:30:48 +0100

    added a simple note

--- src/main/java/com/example/demo/UserOrder.java
+++ src/main/java/com/example/demo/UserOrder.java
@@ -3,6 +3,8 @@ package com.example.demo;
 public class UserOrder {
     /*
      * Now we are just going to stick to few data like customerId, product, quantity
+     * at this moment you can send an order and see what the output. is. Everthing is null right???
+     * nice that was an expected response.
      */
     private String customerId;
     private String product;
