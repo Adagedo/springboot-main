b3eef77facbd8ffe5b2daa158bdaea6e7e49a016
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-31 15:28:01 +0100

    create the order data

--- src/main/java/com/example/demo/UserOrder.java
+++ src/main/java/com/example/demo/UserOrder.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
 public class UserOrder {
-    
+    /*
+     * Now we are just going to stick to few data like customerId, product, quantity
+     */
+    private String customerId;
+    private String product;
+    private int quantity;
 }
