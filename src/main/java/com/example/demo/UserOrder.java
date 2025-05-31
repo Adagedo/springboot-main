2f5eef34ea0c1a82919878362319b60b93a07698
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-31 15:37:57 +0100

    added a note on why we get null outputs

--- src/main/java/com/example/demo/UserOrder.java
+++ src/main/java/com/example/demo/UserOrder.java
@@ -3,8 +3,19 @@ package com.example.demo;
 public class UserOrder {
     /*
      * Now we are just going to stick to few data like customerId, product, quantity
-     * at this moment you can send an order and see what the output. is. Everthing is null right???
+     * at this moment you can send an order and see what the output is. 
+     * example {
+     *              "customerId":"7839jfme9eenve8e",
+     *              "product":"Samsung", 
+     *              "quantity":1
+     *          }
+     * Everthing is null right???
      * nice that was an expected response.
+     * 
+     * Now why are all our data null and 0??? despite getting a data from our users?? 
+     * with our basic understanding of java we can we all Know that this filled we initialized are private filled.
+     * and we dont have acces to private filleds outside the class. 
+     * But can we make them public??? well give a try and see the outcome. but using public is not the ideal approach.
      */
     private String customerId;
     private String product;
